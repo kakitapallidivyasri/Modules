f2c0d69104d11d57ed1ce9978848d150ed2396ac
kakitapallidivyasri <kakitapallidivyasri@gmail.com>
2021-08-18 07:17:51 +0530

    this is node and react

---
+++ hello-world/src/App.js
@@ -0,0 +1,252 @@
+ // import React, { Component,} from 'react';
+//  import logo from './logo.svg';
+//  import './App.css';
+ //import Greet from './components/Greet';
+// import Welcome from './components/Welcome';
+ //import Hello from './components/Hello';
+// import Message from './components/Message';
+// import Counter from './components/Counter';
+//import FunctionClick from './components/FunctionClick'
+ //import ClassClick from './components/ClassClick'
+// import EventBind from './components/EventBind'
+// import ParentComponent from './components/ParentComponent';
+// import UserGreeting from './components/UserGreeting'
+// import NameList from './components/NameList'
+// import Stylesheets from './components/Stylesheets'
+// import Inline from './components/Inline'
+// import Click from './components/Click'
+ //import Counterr from './components/Counterr'
+// import ParentComp from './components/ParentComp'
+// import EvenOdd from './components/EvenOdd'
+// import FormComponent from './components/FormComponent'
+
+
+
+
+ 
+
+//  class App extends Component {
+//   render() {
+  
+//      return (
+//        <div className="App">
+//            {/* <ParentComp></ParentComp>   */}
+//            <Counterr></Counterr>   
+         
+//          {/* <Click></Click>           */}
+//           {/* <Inline></Inline>   */}
+//          {/* <Stylesheets primary = {false}></Stylesheets>  */}
+//          {/* <NameList></NameList>  */}
+//          {/* <UserGreeting></UserGreeting>  */}
+//          {/* <ParentComponent></ParentComponent>  */}
+
+//          {/* <EventBind></EventBind>  */}
+//           {/* <parentComponent></parentComponent>  */}
+//          {/* <Message></Message>   */}
+//          {/* <Counter></Counter>  */}
+//            {/* <FunctionClick></FunctionClick>   */}
+//           {/* <ClassClick></ClassClick>    */}
+//            {/* <Greet Name = 'divya' HeroName = 'nani'>
+//           <p>divya is a good girl</p>
+//           </Greet> 
+//          <Greet Name = 'madhu' HeroName = 'raju'>
+//           <button> Action </button>
+//         </Greet>   
+
+//             <Greet Name = 'reeha' HeroName = 'siva'/> */}
+
+
+        
+//           {/* <Hello></Hello>      */}
+
+//         {/* <Welcome a = {10}></Welcome>  */}
+
+//         {/* <EvenOdd></EvenOdd>  */}
+
+//          {/* <FormComponent></FormComponent>   */}
+
+//         {/* <New></New>  */}
+
+
+       
+//       </div>
+//     );
+//   }
+// }
+
+// export default App
+
+
+// import React from 'react'
+// import logo from './logo.svg'
+// import './App.css';
+// function App(){
+//   return(
+//     <div className = 'App'>
+//       <div className="mb-3" style={{width: +'18rem' }}>
+//   <img src="https://thumbs.dreamstime.com/b/mutton-curry-roast-exotic-indian-spices-homemade-recipes-lamb-slow-cooked-clay-pottery-134960443.jpg" class="card-img-top" alt="..."/>
+//   <div class="card-body">
+//     <h5 class="card-title">Card title</h5>
+//     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//     <a href="https://www.google.com/search?q=chicken+curry+images&rlz=1C1SQJL_enUS919US919&oq=chi&aqs=chrome.0.69i59j69i57j0i395j46i395i433j46i131i395i433j69i60l3.3531j1j9&sourceid=chrome&ie=UTF-8" class="btn btn-primary">Go somewhere</a>
+//   </div>
+// </div>
+// </div>
+//   )
+// }
+// export default App
+
+
+// import React,{Component} from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+// import Button from 'react-bootstrap/Button';
+// class App extends Component{
+//   render(){
+//     return(
+//       <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Hai divya welcome to react world
+//         </p>
+//         <Button>Test it</Button>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//     );
+//   }
+// }
+// export default App;
+
+
+//  import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import {Button , Alert,Card, Breadcrumb, Container,Row,Col ,Image,Figure} from 'react-bootstrap';
+
+// function App(){
+//   return(
+//     <div className='App'>
+//       <header className='App-header'>
+      
+        
+//          <Card.Img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwmRALi-RCd2wYtrdRZWY__72I-DUGfmTh8wMQwr9HQ73-i4ziMaUsjot63ss-G2B4o_U&usqp=CAU'></Card.Img>
+//          <Alert variant='success'>HI i am Divya</Alert>
+//          <Breadcrumb>
+//   <Breadcrumb.Item  href="#">i am GOOD</Breadcrumb.Item>
+//   <Breadcrumb.Item href="https://getbootstrap.com/docs/4.0/components/breadcrumb/">BETTER </Breadcrumb.Item>
+//   <Breadcrumb.Item active>BEST</Breadcrumb.Item>
+//           </Breadcrumb>
+        
+//          <Button> At learning REACT</Button>
+//          <Container>
+//   <Row>
+    
+//     <Col xs={6} md={6}>
+//       <Image src="https://res.cloudinary.com/practicaldev/image/fetch/s--54ca_F2q--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://dev-to-uploads.s3.amazonaws.com/i/1wwdyw5de8avrdkgtz5n.png" thumbnail />
+//     </Col>
+//   </Row>
+// </Container>
+// <Figure>
+//   <Figure.Image
+//     width={200}
+//     height={180}
+//     alt="200x180"
+//     src="https://raddevon.com/wp-content/uploads/2018/10/react.jpg"
+    
+//   />
+//   <Figure.Caption>
+//     THIS IS ANOTHER LOGO
+//   </Figure.Caption>
+// </Figure>
+//       </header>
+
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+//  import React , { Component } from 'react'
+
+// // import logo from './logo.svg';
+// // import './App.css';
+// // import LogIn from './components/LogIn'
+// // //import SignUp from './components/SignUp'
+//  import SignInOutContainer from './Containers';
+
+
+// class App extends Component{ 
+//   render() {
+//    return (
+//     <form className="App">
+//      <div>
+//      <SignInOutContainer />
+     
+//      </div>
+//      </form>
+//      );
+//    }   
+// } 
+// export default App;
+
+
+//form handling
+// import React,{Component} from 'react'
+// import Form from './components/Form'
+// import './App.css';
+// class App extends Component{
+//   render(){
+//     return(
+//       <div className = "App">
+//         <Form/>
+//       </div>
+//     )
+//   }
+// }
+// export default App
+    
+
+// import React,{Component} from 'react'
+// import ParentComp from './components/ParentComp';
+// import './App.css';
+// class App extends Component{
+//   render(){
+//     return(
+//       <div className = 'App'>
+//       <ParentComp/>
+//       </div>
+
+//     )
+//   }
+// }
+// export default App
+
+
+// import React, { Component } from 'react'
+// import './App.css' 
+// import Greet from './components/Greet'
+
+// class App extends Component{
+//   render(){
+//     return(
+//       <div className='App'>
+//         <Greet ></Greet>
+//       </div>
+//     )
+//   }
+  
+// }
+
+// export default App
+
